8b60c7dbea71f6d852d17ea7f342f16db15959dc
andrea longhi <andrea@spaghetticode.it>
2013-04-03 00:21:44 +0200

    delete all friends in seeds.rb

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
+Friend.delete_all
 Friend.create! :name => 'Andrea Longhi'
